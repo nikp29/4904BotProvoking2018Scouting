e2d03d398c1213aa31a3e238d2b578e3ea831cca
nikp29 <nikp29@users.noreply.github.com>
2018-03-29 19:25:33 -0700

    We are ready for SVR!!!(probably)

--- Analytics/TBAAPI.py
+++ Analytics/TBAAPI.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import time
 import httplib2
 import requests
 from bs4 import BeautifulSoup
@@ -11,7 +10,7 @@ from pprint import pprint
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-import datetime
+
 
 try:
     import argparse
@@ -21,6 +20,7 @@ except ImportError:
 teamKey = "frc4904"
 events = []
 data = []
+
 
 class Event():
     """docstring for Event."""
@@ -45,7 +45,7 @@ class Event():
                     redWin = ""
                     blueWin = ""
                 #put all the useful stuff from the Query into one giant dictionary to pass off to the match class
-                matchData.append({"event_name":self.eventName,"event_key":self.eventKey,"number":match["match_number"],"time":match['actual_time'],"red":{"teams":{"1":match['alliances']['red']['team_keys'][0],
+                matchData.append({"event_name":self.eventName,"event_key":self.eventKey,"number":match["match_number"],"time":match['predicted_time'],"red":{"teams":{"1":match['alliances']['red']['team_keys'][0],
                 "2":match['alliances']['red']['team_keys'][1],"3":match['alliances']['red']['team_keys'][2]},"win":redWin,"score":match['alliances']['red']['score']},"blue":{"teams":
                 {"1":match['alliances']['blue']['team_keys'][0],"2":match['alliances']['blue']['team_keys'][1],"3":match['alliances']['blue']['team_keys'][2]},"win":blueWin,"score":match['alliances']['blue']['score']}})
         for match in matchData:
@@ -67,15 +67,17 @@ class Event():
                 self.matches.append(Match(match,"blue",i,teamInfoToPass))
 
 
+
+
 class Match():
     """docstring for Match."""
     def __init__(self,matchDict,teamAlliance,number,teamData):
         matchDict[teamAlliance]['teams'][str(number)] = makeSureNumeric(matchDict[teamAlliance]['teams'][str(number)])
         self.teamData = teamData
-        self.time = matchDict['time']
         self.dictionary = {"event":matchDict['event_name'],"match":matchDict['number'],"team_number":self.teamData['team_number'],"team_name":self.teamData['name'],"alliance":teamAlliance,"robotNumber":number,"W/L":matchDict[teamAlliance]['win'],"score":matchDict[teamAlliance]['score'],
         'dataID':str(matchDict['event_name']+' '+str(matchDict['number'])+' '+str(self.teamData['team_number']))}
         #desired sheet output |event|match|teamNumber|teamName|alliance|robotNumber|win?|score|currentRank(will add on later)
+
 
 def makeSureNumeric(teamKeyString):
     numberPart = teamKeyString[3:]
@@ -87,11 +89,13 @@ def makeSureNumeric(teamKeyString):
     return finalString
 def getKey(filePath):
 
+
     #Get API Key
     tbaFile = open(os.path.expanduser(filePath))
     tbaKey = tbaFile.read()
     tbaFile.close()
     return tbaKey
+
 
 def getQueryFromTBA(suffix):
     #Query TBA's API given url suffix
@@ -104,6 +108,7 @@ def getQueryFromTBA(suffix):
     decodedQuery = query.json()
     return decodedQuery
 
+
 #For trying to do multiple queries
 def getQueriesFromTBA(start,terms,finish):
     queries = []
@@ -111,6 +116,7 @@ def getQueriesFromTBA(start,terms,finish):
         suffix = start + term + finish
         queries.append(getQueryFromTBA(suffix))
     return queries
+
 
 def writeInfotoDataList(eventsList):
     masterData = []
@@ -127,17 +133,13 @@ for eventQuery in eventQueries:
     events.append(Event(eventQuery['key'],eventQuery['name']))
     print(eventQuery['key'])
 
+
 for event in events:
     event.getMatchData()
 
-def writeUpcomingMatchList(eventsList):
-    masterData = []
-    for event in eventsList:
-        export = []
-        for match in event.matches:
-            export.append(match.dictionary)
-        masterData.append(export)
-    return masterData
+
+
+
 #desired sheet output |event|match|teamNumber|teamName|alliance|robotNumber|win?|score|
 data = writeInfotoDataList(events)
 x = 0
@@ -147,7 +149,9 @@ sheetData = [fieldnames]
 for sheet in data:
     x+=1
 
+
     exportableData = []
+
 
     for match in sheet:
         listFromDict = []
@@ -157,7 +161,8 @@ for sheet in data:
     finalData= finalData +exportableData
     sheetData= sheetData +exportableData
 df = pd.DataFrame(columns = fieldnames,data=finalData)
-#print(df)
+print(df)
+
 
 """ This is code to generate a list of teams at an event
 for event in events:
@@ -169,16 +174,7 @@ dfSFTeams = pd.DataFrame(columns = ["teamKey"],data=teamList)
 df.to_csv('teamData.csv')
 #/event/{event_key}/teams/keys """
 
-def getNextMatch(eventcode):
-    nextMatch = ""
-    matchList = getQueryFromTBA("/event/"+"2018casj"+"/matches/simple")
-    matchList.sort(key=lambda x: x['match_number'])
-    for match in matchList:
 
-        if str(match['actual_time']) == "None":
-            if nextMatch == "":
-                nextMatch = match
-    return nextMatch
 
 
 #Now the pushing to API
@@ -187,6 +183,8 @@ CLIENT_SECRET_FILE = os.path.join(os.environ['HOME'],'Documents/Programming/Scou
 APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 # TODO: Change placeholder below to generate authentication credentials. See
 # https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
+
+
 
 
 def get_credentials():
@@ -205,6 +203,7 @@ def get_credentials():
     credential_path = os.path.join(credential_dir,
                                    'sheets.googleapis.com-python-quickstart.json')
 
+
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
@@ -214,10 +213,12 @@ def get_credentials():
             credentials = tools.run_flow(flow, store, flags)
         else: # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
-        #print('Storing credentials to ' + credential_path)
+        print('Storing credentials to ' + credential_path)
     return credentials
 
+
 def main(spreadsheet_id, pushData):
+
 
     """Shows basic usage of the Sheets API.
 
@@ -228,20 +229,30 @@ def main(spreadsheet_id, pushData):
 
 
 
+
+
+
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
 
 
+
+
     service = discovery.build('sheets', 'v4', credentials=credentials)
+
+
 
 
     # The A1 notation of the values to update.
     range_ = 'A1:AZ3000'
 
+
     # How the input data should be interpreted.
     value_input_option = 'USER_ENTERED'
+
+
 
 
     valuetoencode = {
@@ -249,21 +260,24 @@ def main(spreadsheet_id, pushData):
         "values": pushData
     }
 
+
     value_range_body = json.dumps(valuetoencode)
-    #print(value_range_body)
+    print(value_range_body)
     request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=valuetoencode)
     response = request.execute()
 
+
     # TODO: Change code below to process the `response` dict:
-    #pprint(response)
+    pprint(response)
+
 
 #Team match data table
+
 
 # data = {'qual1': {'red': [6658, 5572, 5190], 'blue': [...]}}
 
 
+
+
 if __name__ == '__main__':
     main('1QTdG8EFg7ob5qtzc7f6aGbVuh8vdr_faDxOfTEO1VbY',sheetData)
-while True:
-    print(getNextMatch("2018cada"))
-    time.sleep(10)
